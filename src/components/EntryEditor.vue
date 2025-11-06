3e7d50b8f8d9393f5e521f1dc5f76c17419fac70
Guilherme-Ferreti <guiferretis@gmail.com>
2025-11-06 11:20:53 -0300

    feat: add validation to entry form

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -3,14 +3,22 @@
     class="mt-10 rounded-md bg-gray-200 p-5 dark:bg-slate-800"
     @submit.prevent="handleSubmit"
   >
-    <textarea
-      v-model="body"
-      class="mb-5 h-40 w-full resize-none rounded-md p-5 dark:bg-slate-900"
-      placeholder="New Journal Entry for johndoe_io"
-    ></textarea>
-    <EmojiField v-model="emoji" />
+    <div class="mb-5">
+      <textarea
+        v-model="body"
+        class="h-40 w-full resize-none rounded-md p-5 dark:bg-slate-900"
+        placeholder="New Journal Entry for johndoe_io"
+        maxlength="280"
+        autofocus
+      ></textarea>
+      <FieldError v-if="bodyHasError">Please provide an entry.</FieldError>
+    </div>
+    <div>
+      <EmojiField v-model="emoji" />
+      <FieldError v-if="emojiHasError">Please select and emoji.</FieldError>
+    </div>
     <div class="flex justify-between">
-      <span>0 / 280</span>
+      <span>{{ body.length }} / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
@@ -23,33 +31,45 @@ import { ref } from 'vue';
 import type { Emoji } from '@/types/Emoji';
 import type Entry from '@/types/Entry';
 import { nanoid } from 'nanoid';
+import FieldError from './FieldError.vue';
 
 const body = ref('');
 const emoji = ref<Emoji | null>(null);
+
+const bodyHasError = ref(false);
+const emojiHasError = ref(false);
 
 const emit = defineEmits<{
   created: [entry: Entry];
 }>();
 
 function handleSubmit() {
-  if (body.value.length <= 0) {
-    alert('Please provide an entry.');
-    return;
-  }
-
-  if (emoji.value === null) {
-    alert('Please select an emoji.');
+  if (!validateForm()) {
     return;
   }
 
   emit('created', {
     id: nanoid(),
     body: body.value,
-    emoji: emoji.value,
+    emoji: emoji.value as Emoji,
     createdAt: new Date(),
   });
 
+  resetForm();
+}
+
+function validateForm() {
+  bodyHasError.value = body.value.length <= 0;
+  emojiHasError.value = emoji.value === null;
+
+  return !bodyHasError.value && !emojiHasError.value;
+}
+
+function resetForm() {
   body.value = '';
   emoji.value = null;
+
+  bodyHasError.value = false;
+  emojiHasError.value = false;
 }
 </script>
